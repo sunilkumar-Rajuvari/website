df26f44f050e1a81695d6cf1a88d5a79000b7b44
sunilkumar-Rajuvari <rajuvarisunil@gmail.com>
2024-10-21 23:45:41 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.s1').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: false,
+        arrows: false
+    });
+});
+$(document).ready(function () {
+    $('.s2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: false,
+        arrows: false
+    });
+});
